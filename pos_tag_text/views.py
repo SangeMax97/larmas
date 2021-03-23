51e1849a9638adae454f3403c50ef3cd3dc07a41
Sange Maxaku <mxksan002@myuct.ac.za>
2021-03-23 15:22:41 +0200

    fixing pos tagger when parsing text to business logic

--- pos_tag_text/views.py
+++ pos_tag_text/views.py
@@ -10,25 +10,26 @@ def index(request):
         language = request.POST.get('language', '')
         text = request.POST.get('text', '').lower().split()
 
-        tagged = []
+        tagged = {}
 
         x = Language.objects.filter(text__in = text)
+        
         for i in x:
             try:    
-                print(i.text_metadata.first().part_of_speech)
-
                 y = '<div class="tag-label tagged '+ i.text_metadata.first().part_of_speech + '">' + i.text + ' <div class="badge badge-light">' + i.text_metadata.first().part_of_speech + '</div></div>'
             except:
                 y = '<div class="tag-label">' + i.text + '</div>'
 
-            text = list(map(lambda b: b.replace(i.text,y), text))
+            tagged[i.text] = y
 
-        text = list(map(lambda x: x.replace(x, '<div class="tag-label">' + x + '</div>') if '</div>' not in x else x, text))
-
+        for i in range(len(text)):
+            try:
+                text[i] = tagged[text[i]]
+            except:
+                text[i] = '<div class="tag-label">' + text[i] + '</div>'
+                
         context = {'response': text}
-
+        
         return JsonResponse(context)
-
-
-
+        
     return render(request, 'pos_tag_text/pos_tag.html')
